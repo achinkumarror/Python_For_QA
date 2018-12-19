217219a09e2b8e2604c71460fd8ce76e4bacd83d
achinror <45988091+achinror@users.noreply.github.com>
2018-12-19 11:42:10 +0530

    Add files via upload

---
+++ UserInput1.py
@@ -0,0 +1,10 @@
+a = input("Type Here:")
+print(a)
+print(type(a))
+
+#########################################
+a=input("Type Here:")
+b=input("Type here:")
+print(a+b)
+
+#########################################
---
+++ UserInput2.py
@@ -0,0 +1,3 @@
+a=int(input("Type Here:"))
+b=int(input("Type here:"))
+print(a+b)
