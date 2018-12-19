57f3b3a08fce2f09899b2530b9fa891263d8842f
Achin Ror <achinkumarror@gmail.com>
2018-12-19 12:22:48 +0530

    Add files via upload

---
+++ PyString1.py
@@ -0,0 +1,16 @@
+#String in Python
+
+str = " achin Ror is at Python world "
+
+print(str.__len__())
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.lstrip())
+print(str.rstrip())
+print(str.strip())
+print(str[0])
+print(str[2:8])
+print(str[2:])
+print(str*2)
+print(str + "Concat love from pyhton 3")
